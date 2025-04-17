334611f0639b328331b016e68e1962410148f2c4
L Davison <92911465+ldavison-sg@users.noreply.github.com>
2025-04-17 08:55:36 +0000

    It is possible to directly link to a .sass CSS file and have the vite preprocessor generate the CSS at build time. This is working, though the design system .sass files use a lot of deprecated syntax so there are many warnings from the sass processor.

--- test-app/vite.config.ts
+++ test-app/vite.config.ts
@@ -4,6 +4,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	css: {
+		preprocessorOptions: {
+			sass: {
+				quietDeps: true
+			}
+		}
+	},
 	test: {
 		workspace: [
 			{
