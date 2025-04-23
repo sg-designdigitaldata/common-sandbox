37e5e3924a433abf4b85c61b0abe29f69ffb523d
neilmccbad <95418728+neilmccbad@users.noreply.github.com>
2025-04-23 13:17:28 +0000

    added feature component for postcode search

--- postcode-search/src/app/app.component.ts
+++ postcode-search/src/app/app.component.ts
@@ -2,32 +2,34 @@ import { Component } from '@angular/core';
 import axios from 'axios';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import  { PostcodeSearchComponent } from './postcode-search/postcode-search.component';
 
 @Component({
   standalone:true,
   selector: 'app-root',
-  imports: [CommonModule, FormsModule],
+  template: `<app-postcode-search></app-postcode-search>`,
+  imports: [CommonModule, FormsModule, PostcodeSearchComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  postcode: string = '';
-  addresses: string[] = [];
-  apiKey: string = 'nLWMMF3c567DLus9Tc1QVpAQzlOuBPji';
+  // postcode: string = '';
+  // addresses: string[] = [];
+  // apiKey: string = 'nLWMMF3c567DLus9Tc1QVpAQzlOuBPji';
 
-  async search() {
-    if (!this.postcode.trim()) return;
+  // async search() {
+  //   if (!this.postcode.trim()) return;
 
-    const url = `https://api.os.uk/search/places/v1/postcode?postcode=${encodeURIComponent(this.postcode)}&key=${this.apiKey}`;
+  //   const url = `https://api.os.uk/search/places/v1/postcode?postcode=${encodeURIComponent(this.postcode)}&key=${this.apiKey}`;
 
-    try {
-      const response = await axios.get(url);
-      const results = response.data.results;
+  //   try {
+  //     const response = await axios.get(url);
+  //     const results = response.data.results;
 
-      this.addresses = results?.map((res: any) => res.DPA.ADDRESS) || [];
-    } catch (err) {
-      console.error('Error:', err);
-      this.addresses = ['Error retrieving data.'];
-    }
-  }
+  //     this.addresses = results?.map((res: any) => res.DPA.ADDRESS) || [];
+  //   } catch (err) {
+  //     console.error('Error:', err);
+  //     this.addresses = ['Error retrieving data.'];
+  //   }
+  // }
 }
--- postcode-search/src/app/postcode-search/postcode-search.component.ts
+++ postcode-search/src/app/postcode-search/postcode-search.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import axios from 'axios';
 
 @Component({
+  standalone: true,
   selector: 'app-postcode-search',
-  imports: [],
   templateUrl: './postcode-search.component.html',
-  styleUrl: './postcode-search.component.css'
+  styleUrls: ['./postcode-search.component.css'],
+  imports: [CommonModule, FormsModule]
 })
 export class PostcodeSearchComponent {
+  postcode: string = '';
+  addresses: string[] = [];
+  apiKey: string = 'nLWMMF3c567DLus9Tc1QVpAQzlOuBPji';
+
+  async search() {
+    if (!this.postcode.trim()) return;
+
+    const url = `https://api.os.uk/search/places/v1/postcode?postcode=${encodeURIComponent(this.postcode)}&key=${this.apiKey}`;
+
+    try {
+      const response = await axios.get(url);
+      const results = response.data.results;
+
+      this.addresses = results?.map((res: any) => res.DPA.ADDRESS) || [];
+    } catch (err) {
+      console.error('Error:', err);
+      this.addresses = ['Error retrieving data.'];
+    }
+  }
 
 }
+
